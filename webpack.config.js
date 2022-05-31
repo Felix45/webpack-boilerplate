ddf4d7a0b327be20a2d29fd25976beb33a20b5f9
Felix Ouma <fatonoh@gmail.com>
2022-05-31 16:06:37 +0300

    Add webpack css loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,27 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    'index': './src/index.js',
+    'print': './src/print.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
 };
